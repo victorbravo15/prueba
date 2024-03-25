23e32738812bf55d0d5774a6a91268b7264ca3e9
Víctor Bravo <32301185+victorbravo15@users.noreply.github.com>
2024-03-25 19:36:49 +0100

    Add files via upload

---
+++ WebTreeView/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using WebTreeView2.Models;
+
+namespace WebTreeView2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string json = await System.IO.File.ReadAllTextAsync("Files\\Items.json");
+            var items = JsonSerializer.Deserialize<List<Item>>(json);
+            return View(items);
+        }
+    }
+}
